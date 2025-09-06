32cd27b6c269aef400e832478a61589ece8ef278
devMago23 <alexandremg2347@gmail.com>
2025-09-06 18:47:45 -0300

    funcao para ordenar tarefas fixadas

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ const noteInput = document.querySelector("#note-content");
 const addBtn = document.querySelector(".add-note");
 
 function showNotes() {
+    cleanNotes();
     getNotes().forEach((note) => {
         const noteElement = createNote(note.id, note.content, note.fixed)
         notesContainer.appendChild(noteElement);
@@ -10,6 +11,10 @@ function showNotes() {
     });
 }
  
+function cleanNotes() {
+    notesContainer.replaceChildren([])
+}
+
 function addNote() {
     const notes = getNotes();
     const noteOBject = {
@@ -25,6 +30,7 @@ function addNote() {
 
     saveNote(notes);
     
+    
     noteInput.value ="";
 }
 
@@ -35,20 +41,49 @@ function generateId() {
 function createNote(id, content, fixed) {
    const element = document.createElement("div");
    element.classList.add("note");
-  console.log(`qntd de container com classe note ${document.querySelectorAll(".note").length}`);
   const textArea = document.createElement("textarea");
 
   textArea.value = content;
   textArea.placeholder = "Insira algum texto";
 
   element.appendChild(textArea);
-  return element;
-  
+
+  const pinIcon = document.createElement("i");
+  pinIcon.classList.add(...["bi", "bi-pin"]);
+    element.appendChild(pinIcon);
+    
+    if(fixed){
+        element.classList.add("fixed")
+    }
+
+    element.querySelector(".bi-pin").addEventListener("click", ()=>{
+        togleFixedNotes(id);
+    })
+    
+    return element;
 }
+
+function togleFixedNotes(id) {
+
+    const notes = getNotes();
+    const targetNotes = notes.filter((notes)=> notes.id === id)[0]
+
+    // se tiver fixado o botao vai desfixar 
+    targetNotes.fixed = !targetNotes.fixed;
+    
+    saveNote(notes);
+    showNotes();
+    
+}
+
 
 function getNotes() {
     const notes = JSON.parse(localStorage.getItem("notes")|| "[]");
-    return notes;
+    
+    // aplica um algoritmo de ordenação 
+    const orderedNotes = notes.sort((a,b) => a.fixed > b.fixed ? -1: 1);
+
+    return orderedNotes;
 }
 
 function saveNote(notes) {
