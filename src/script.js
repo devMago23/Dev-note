5c326937569f249fb5b3119ff1b6c405b275e824
devMago23 <alexandremg2347@gmail.com>
2025-09-06 16:47:49 -0300

    salvando notas na localstorage

--- src/script.js
+++ src/script.js
@@ -2,25 +2,36 @@ const notesContainer = document.querySelector("#note-container");
 const noteInput = document.querySelector("#note-content");
 const addBtn = document.querySelector(".add-note");
 
+function showNotes() {
+    getNotes().forEach((note) => {
+        const noteElement = createNote(note.id, note.content, note.fixed)
+        notesContainer.appendChild(noteElement);
+        
+    });
+}
  
 function addNote() {
-    
+    const notes = getNotes();
     const noteOBject = {
        id:generateId(),
        content: noteInput.value,
-       fixed: false
+       fixed: false,
     };
    
     const noteElement = createNote(noteOBject.id, noteOBject.content);
-    notesContainer.appendChild(noteElement)
+    notesContainer.appendChild(noteElement);
 
+    notes.push(noteOBject);
+
+    saveNote(notes);
+    
+    noteInput.value ="";
 }
 
 function generateId() {
     return Math.floor(Math.random()*2000)
 }
 
-let ele = 0;
 function createNote(id, content, fixed) {
    const element = document.createElement("div");
    element.classList.add("note");
@@ -35,5 +46,15 @@ function createNote(id, content, fixed) {
   
 }
 
+function getNotes() {
+    const notes = JSON.parse(localStorage.getItem("notes")|| "[]");
+    return notes;
+}
+
+function saveNote(notes) {
+    localStorage.setItem("notes",JSON.stringify(notes));
+}
 
 addBtn.addEventListener("click", () => addNote())
+
+showNotes();
